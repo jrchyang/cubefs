4e68f36636a563029bd87fde498aca38bdf5be14
jrchyang <yujrchyang@gmail.com>
2025-03-12 09:29:00 +0800

    add offline build

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 RootPath=$(cd $(dirname $0); pwd)
+module=$1
 
 build_linux_x86_64() {
-     make
+     make "$module"
 }
 
 # build arm64 with amd64 docker ubuntu:focal, apt-get install -y gcc-9-aarch64-linux-gnu gcc-aarch64-linux-gnu  g++-9-aarch64-linux-gnu g++-aarch64-linux-gnu
@@ -17,7 +18,7 @@ build_linux_arm64_gcc9() {
     export EXTRA_CFLAGS="-Wno-error=deprecated-copy -fno-strict-aliasing -Wclass-memaccess -Wno-error=class-memaccess -Wpessimizing-move -Wno-error=pessimizing-move"
     export EXTRA_CXXFLAGS=$EXTRA_CFLAGS
 
-    CGO_ENABLED=1 GOOS=linux GOARCH=arm64 make
+    CGO_ENABLED=1 GOOS=linux GOARCH=arm64 make "$module"
 }
 
 # build arm64 with amd64 docker buntu:xenial , apt-get install -y gcc-4.9-aarch64-linux-gnu gcc-aarch64-linux-gnu  g++-4.9-aarch64-linux-gnu g++-aarch64-linux-gnu
@@ -32,7 +33,7 @@ build_linux_arm64_gcc4() {
     export EXTRA_CFLAGS=" -fno-strict-aliasing  "
     export EXTRA_CXXFLAGS=$EXTRA_CFLAGS
 
-    CGO_ENABLED=1 GOOS=linux GOARCH=arm64 make
+    CGO_ENABLED=1 GOOS=linux GOARCH=arm64 make "$module"
 }
 
 # wget compress dep
@@ -46,29 +47,44 @@ get_rocksdb_compress_dep() {
     cd ${RootPath}/vendor/dep
 
     if [ ! -d "${RootPath}/vendor/dep/zlib-1.2.11" ]; then
-        wget https://zlib.net/fossils/zlib-1.2.11.tar.gz
+        if [ -n "$OFFLINE_BUILD" ];then
+            cp /home/cubefs-deps/zlib-1.2.11.tar.gz ./
+        else
+            wget https://zlib.net/fossils/zlib-1.2.11.tar.gz
+        fi
         tar zxf zlib-1.2.11.tar.gz
     fi
 
     if [ ! -d "${RootPath}/vendor/dep/bzip2-1.0.6" ]; then
-        wget https://sourceforge.net/projects/bzip2/files/bzip2-1.0.6.tar.gz
+        if [ -n "$OFFLINE_BUILD" ];then
+            cp /home/cubefs-deps/bzip2-1.0.6.tar.gz ./
+        else
+            wget https://sourceforge.net/projects/bzip2/files/bzip2-1.0.6.tar.gz
+        fi
         tar zxf bzip2-1.0.6.tar.gz
     fi
 
     if [ ! -d "${RootPath}/vendor/dep/zstd-1.4.8" ]; then
-        wget https://codeload.github.com/facebook/zstd/zip/v1.4.8
-        unzip v1.4.8
+        if [ -n "$OFFLINE_BUILD" ];then
+            cp /home/cubefs-deps/zstd-v1.4.8.zip ./
+        else
+            wget -O zstd-v1.4.8.zip https://codeload.github.com/facebook/zstd/zip/v1.4.8
+        fi
+        unzip zstd-v1.4.8.zip
     fi
 
     if [ ! -d "${RootPath}/vendor/dep/lz4-1.9.3" ]; then
-        wget https://codeload.github.com/lz4/lz4/tar.gz/v1.9.3
-        tar zxf v1.9.3
+        if [ -n "$OFFLINE_BUILD" ];then
+            cp /home/cubefs-deps/lz4-v1.9.3.tar.gz ./
+        else
+            wget -O lz4-v1.9.3.tar.gz https://codeload.github.com/lz4/lz4/tar.gz/v1.9.3
+        fi
+        tar zxf lz4-v1.9.3.tar.gz
     fi
 
     #rm -rf zlib-1.2.11.tar.gz bzip2-1.0.6.tar.gz v1.4.8 v1.9.3
     cd ${RootPath}
 }
-
 
 CPUTYPE=${CPUTYPE} | tr 'A-Z' 'a-z'
 echo ${CPUTYPE}
