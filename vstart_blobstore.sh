48657fd2b8864a6c97b15bfcc726044dd13b7f56
jrchyang <yujrchyang@gmail.com>
2025-03-14 20:47:21 +0800

    update blobstore rpm build

--- vstart_blobstore.sh
+++ vstart_blobstore.sh
@@ -90,9 +90,9 @@ if [ ${num} -le 1 ];then
 fi
 
 # Start the clustermgr
-nohup /usr/bin/clustermgr -f /usr/bin/blobstore/clustermgr1.conf >> /var/log/blobstore/clustermgr1.log  2>&1 &
-nohup /usr/bin/clustermgr -f /usr/bin/blobstore/clustermgr2.conf >> /var/log/blobstore/clustermgr2.log  2>&1 &
-nohup /usr/bin/clustermgr -f /usr/bin/blobstore/clustermgr3.conf >> /var/log/blobstore/clustermgr3.log  2>&1 &
+nohup /usr/bin/clustermgr -f /usr/bin/blobstore/sample.clustermgr1.conf >> /var/log/blobstore/clustermgr1.log  2>&1 &
+nohup /usr/bin/clustermgr -f /usr/bin/blobstore/sample.clustermgr2.conf >> /var/log/blobstore/clustermgr2.log  2>&1 &
+nohup /usr/bin/clustermgr -f /usr/bin/blobstore/sample.clustermgr3.conf >> /var/log/blobstore/clustermgr3.log  2>&1 &
 sleep 5
 num=$(ps -ef | egrep /usr/bin/clustermgr |  egrep -v "vi|tail|grep" | wc -l)
 if [ $num -ne 3 ]; then
@@ -104,7 +104,7 @@ sleep 15
 echo "start clustermgr ok"
 
 # Start the proxy
-nohup /usr/bin/proxy -f /usr/bin/blobstore/proxy.conf >> /var/log/blobstore/logs/proxy.log 2>&1 &
+nohup /usr/bin/proxy -f /usr/bin/blobstore/sample.proxy.conf >> /var/log/blobstore/logs/proxy.log 2>&1 &
 sleep 1
 num=$(ps -ef | egrep /usr/bin/proxy |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
@@ -114,7 +114,7 @@ fi
 echo "start proxy ok"
 
 # Start the scheduler
-nohup /usr/bin/scheduler -f /usr/bin/blobstore/scheduler.conf >> /var/log/blobstore/scheduler.log 2>&1 &
+nohup /usr/bin/scheduler -f /usr/bin/blobstore/sample.scheduler.conf >> /var/log/blobstore/scheduler.log 2>&1 &
 sleep 1
 num=$(ps -ef | egrep /usr/bin/scheduler |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
@@ -125,7 +125,7 @@ echo "start scheduler ok"
 
 mkdir -p /var/run/blobstore/disks/disk{1..8}
 # Start the blobnode
-nohup /usr/bin/blobnode -f /usr/bin/blobstore/blobnode.conf >> /var/log/blobstore/blobnode.log 2>&1 &
+nohup /usr/bin/blobnode -f /usr/bin/blobstore/sample.blobnode.conf >> /var/log/blobstore/blobnode.log 2>&1 &
 sleep 1
 num=$(ps -ef | egrep /usr/bin/blobnode |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
@@ -140,7 +140,7 @@ if [ "${consul}" == "--consul" ]; then
 fi
 
 # Start the access
-nohup /usr/bin/access -f /usr/bin/blobstore/access.conf >> /var/log/blobstore/access.log 2>&1 &
+nohup /usr/bin/access -f /usr/bin/blobstore/sample.access.conf >> /var/log/blobstore/access.log 2>&1 &
 sleep 1
 num=$(ps -ef | egrep /usr/bin/access |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
