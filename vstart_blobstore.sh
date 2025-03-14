0154d5a9777de7aafda3c2f72579f8090838511a
jrchyang <yujrchyang@gmail.com>
2025-03-14 20:35:41 +0800

    update blobstore rpm build

--- vstart_blobstore.sh
+++ vstart_blobstore.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-set -e
-
 source ./check_env.sh
 
-bin_dir="/usr/bin"
-dep_dir="/home/blobstore-deps"
-dep_bin_dir="/usr/bin/blobstore"
-sample_conf_dir="/usr/bin/blobstore"
-log_dir="/var/log/blobstore"
-run_dir="/var/run/blobstore"
-cpu_arch=$(check_cpu_architecture)
-consul=$1
-
-function INIT()
+function setup_dependences()
 {
     # get consul
     if [ ! -f "$dep_bin_dir/consul" ]; then
@@ -23,10 +12,10 @@ function INIT()
             consul_name="consul_1.11.4_linux_arm64.zip"
         fi
 
-        cd "$dep_dir"
+        cd "$dep_dir" || exit
         unzip -q "$consul_name"
         mv consul "$dep_bin_dir/"
-        cd -
+        cd - || exit
     fi
 
     # get kafka
@@ -38,9 +27,9 @@ function INIT()
             jdk_name="jdk-8u321-linux-aarch64.tar.gz"
         fi
 
-        cd "$dep_dir"
+        cd "$dep_dir" || exit
         tar -zxf "$jdk_name" -C "$dep_bin_dir/"
-        cd -
+        cd - || exit
     fi
 
     # init java
@@ -57,19 +46,22 @@ function INIT()
 
     if [ ! -d "$dep_bin_dir/kafka_2.13-3.1.0" ]; then
         kafka_name="kafka_2.13-3.1.0.tgz"
-        cd "$dep_dir"
+        cd "$dep_dir" || exit
         tar -zxf "$kafka_name" -C "$dep_bin_dir/"
-        cd -
+        cd - || exit
     fi
 }
 
-if [ ! -d "$log_dir" ];then
-    mkdir -p "$log_dir"
-fi
+dep_dir="/home/blobstore-deps"
+dep_bin_dir="/usr/bin/blobstore"
+cpu_arch=$(get_cpu_architecture)
+consul=$1
+
+setup_dependences
 
 # start consul
 if [ "${consul}" == "--consul" ]; then
-    nohup "$dep_bin_dir"/consul agent -dev -client 0.0.0.0 >> "$log_dir"/consul.log 2>&1 &
+    nohup /usr/bin/blobstore/consul agent -dev -client 0.0.0.0 >> /var/log/blobstore/consul.log 2>&1 &
     # check consul running
     sleep 1
     num=$(ps -ef | egrep "$dep_bin_dir/consul" | egrep -v "grep|vi|tail" | wc -l)
@@ -98,11 +90,11 @@ if [ ${num} -le 1 ];then
 fi
 
 # Start the clustermgr
-nohup "$bin_dir"/clustermgr -f "$sample_conf_dir"/clustermgr1.conf >> "$log_dir"/clustermgr1.log  2>&1 &
-nohup "$bin_dir"/clustermgr -f "$sample_conf_dir"/clustermgr2.conf >> "$log_dir"/clustermgr2.log  2>&1 &
-nohup "$bin_dir"/clustermgr -f "$sample_conf_dir"/clustermgr3.conf >> "$log_dir"/clustermgr3.log  2>&1 &
+nohup /usr/bin/blobstore/clustermgr -f /usr/bin/blobstore/clustermgr1.conf >> /var/log/blobstore/clustermgr1.log  2>&1 &
+nohup /usr/bin/blobstore/clustermgr -f /usr/bin/blobstore/clustermgr2.conf >> /var/log/blobstore/clustermgr2.log  2>&1 &
+nohup /usr/bin/blobstore/clustermgr -f /usr/bin/blobstore/clustermgr3.conf >> /var/log/blobstore/clustermgr3.log  2>&1 &
 sleep 5
-num=$(ps -ef | egrep "$bin_dir/clustermgr" |  egrep -v "vi|tail|grep" | wc -l)
+num=$(ps -ef | egrep /usr/bin/blobstore/clustermgr |  egrep -v "vi|tail|grep" | wc -l)
 if [ $num -ne 3 ]; then
     echo "Failed to start clustermgr"
     exit 1
@@ -112,9 +104,9 @@ sleep 15
 echo "start clustermgr ok"
 
 # Start the proxy
-nohup "$bin_dir"/proxy -f "$sample_conf_dir"/proxy.conf >> "$log_dir"/logs/proxy.log 2>&1 &
+nohup /usr/bin/blobstore/proxy -f /usr/bin/blobstore/proxy.conf >> /var/log/blobstore/logs/proxy.log 2>&1 &
 sleep 1
-num=$(ps -ef | egrep "$bin_dir"/proxy |  egrep -v "vi|tail|grep" | wc -l)
+num=$(ps -ef | egrep /usr/bin/blobstore/proxy |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
     echo "The proxy start failed."
     exit 1
@@ -122,20 +114,20 @@ fi
 echo "start proxy ok"
 
 # Start the scheduler
-nohup "$bin_dir"/scheduler -f "$sample_conf_dir"/scheduler.conf >> "$log_dir"/scheduler.log 2>&1 &
+nohup /usr/bin/blobstore/scheduler -f /usr/bin/blobstore/scheduler.conf >> /var/log/blobstore/scheduler.log 2>&1 &
 sleep 1
-num=$(ps -ef | egrep "$bin_dir"/scheduler |  egrep -v "vi|tail|grep" | wc -l)
+num=$(ps -ef | egrep /usr/bin/blobstore/scheduler |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
     echo "The scheduler start failed."
     exit 1
 fi
 echo "start scheduler ok"
 
-mkdir -p "$run_dir"/disks/disk{1..8}
+mkdir -p /var/run/blobstore/disks/disk{1..8}
 # Start the blobnode
-nohup "$bin_dir"/blobnode -f "$sample_conf_dir"/blobnode.conf >> "$log_dir"/blobnode.log 2>&1 &
+nohup /usr/bin/blobstore/blobnode -f /usr/bin/blobstore/blobnode.conf >> /var/log/blobstore/blobnode.log 2>&1 &
 sleep 1
-num=$(ps -ef | egrep "$bin_dir"/blobnode |  egrep -v "vi|tail|grep" | wc -l)
+num=$(ps -ef | egrep /usr/bin/blobstore/blobnode |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
     echo "The blobnode start failed."
     exit 1
@@ -148,9 +140,9 @@ if [ "${consul}" == "--consul" ]; then
 fi
 
 # Start the access
-nohup "$bin_dir"/access -f "$sample_conf_dir"/access.conf >> "$log_dir"/access.log 2>&1 &
+nohup /usr/bin/blobstore/access -f /usr/bin/blobstore/access.conf >> /var/log/blobstore/access.log 2>&1 &
 sleep 1
-num=$(ps -ef | egrep "$bin_dir"/access |  egrep -v "vi|tail|grep" | wc -l)
+num=$(ps -ef | egrep /usr/bin/blobstore/access |  egrep -v "vi|tail|grep" | wc -l)
 if [ ${num} -lt 1 ];then
     echo "The access start failed."
     exit 1
